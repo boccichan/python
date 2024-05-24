724c2499486a65348634efb0c6b6009a0d5d54aa
aalpha <153364106+boccichan@users.noreply.github.com>
2024-05-24 11:01:55 +0800

    Add files via upload

---
+++ 第一題.py
@@ -0,0 +1,11 @@
+for i in range (5) :            # 兩行算一組
+    for j in range (10) :       # 每一組的第一行
+        print("*+", end = '')   # 輸出第一行裡的字 '*+' 為一個unit
+    print()                     # 換行
+    print()                     # 換行
+    for k in range (10) :       # 每一組的第二行
+        print("+*", end = '')   # 輸出第二行裡的字 '+*' 為一個unit
+    print()                     # 換行
+    print()                     # 換行
+    
+
